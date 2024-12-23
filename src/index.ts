b9a6e6024db7850f57b4ed29766b3311dbd97d45
Logan <loganzartman@live.com>
2024-12-23 17:51:27 -0600

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { component } from './component';
 import { ecs } from './ecs';
+import { event } from './event';
+import { keyLookup } from './keyLookup';
 import { query } from './query';
 
 export * from './component';
 export * from './ecs';
+export * from './event';
 export * from './keyLookup';
 export * from './query';
 
-const zecs = { component, ecs, query };
+const zecs = { component, ecs, event, keyLookup, query };
 export default zecs;
