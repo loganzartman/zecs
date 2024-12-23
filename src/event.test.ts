f087723ddee620bb372d1f194479b8d4e9fc19f3
Logan <loganzartman@live.com>
2024-12-23 17:46:13 -0600

    tiny events

---
+++ src/event.test.ts
@@ -0,0 +1,42 @@
+import { z } from 'zod';
+import { event } from './event';
+
+describe('events', () => {
+  it('can subscribe to events', () => {
+    const scoreEvent = event('score', z.object({ points: z.number() }));
+    scoreEvent.on((params) => {
+      expect(params.points).toBe(10);
+    });
+    scoreEvent.emit({ points: 10 });
+  });
+
+  it('can unsubscribe from events', () => {
+    const scoreEvent = event('score', z.object({ points: z.number() }));
+    const off = scoreEvent.on((params) => {
+      expect(params.points).toBe(10);
+    });
+    off();
+    scoreEvent.emit({ points: 10 });
+  });
+
+  it('can subscribe to events once', () => {
+    const scoreEvent = event('score', z.object({ points: z.number() }));
+    scoreEvent.once((params) => {
+      expect(params.points).toBe(10);
+      expect(params.points).not.toBe(20);
+    });
+    scoreEvent.emit({ points: 10 });
+    scoreEvent.emit({ points: 20 });
+  });
+
+  it('can have multiple listeners', () => {
+    const scoreEvent = event('score', z.object({ points: z.number() }));
+    const listener1 = jest.fn();
+    const listener2 = jest.fn();
+    scoreEvent.on(listener1);
+    scoreEvent.on(listener2);
+    scoreEvent.emit({ points: 10 });
+    expect(listener1).toHaveBeenCalledTimes(1);
+    expect(listener2).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/event.ts
@@ -0,0 +1,55 @@
+import type { z, ZodTypeAny } from 'zod';
+
+declare const eventNameSymbol: unique symbol;
+
+type Off = () => void;
+
+export type Listener<TName extends string, TParams extends ZodTypeAny> = {
+  [eventNameSymbol]?: TName;
+} & ((params: z.infer<TParams>) => void);
+
+export type ListenerEventType<TListener extends Listener<string, ZodTypeAny>> =
+  TListener extends Listener<infer TName, infer TParams>
+    ? EventType<TName, TParams>
+    : never;
+
+class EventType<TName extends string, TParams extends ZodTypeAny> {
+  name: TName;
+  params: TParams;
+  #listeners = new Set<Listener<TName, TParams>>();
+
+  constructor(name: TName, params: TParams) {
+    this.name = name;
+    this.params = params;
+  }
+
+  on(listener: Listener<TName, TParams>): Off {
+    this.#listeners.add(listener);
+    return () => this.off(listener);
+  }
+
+  off(listener: Listener<TName, TParams>): void {
+    this.#listeners.delete(listener);
+  }
+
+  once(listener: Listener<TName, TParams>): Off {
+    const off = this.on((params) => {
+      off();
+      listener(params);
+    });
+    return off;
+  }
+
+  emit(params: z.infer<TParams>) {
+    for (const listener of this.#listeners) {
+      listener(params);
+    }
+  }
+}
+
+export function event<TName extends string, TParams extends ZodTypeAny>(
+  name: TName,
+  params: TParams,
+): EventType<TName, TParams> {
+  return new EventType(name, params);
+}
