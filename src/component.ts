cc8cdead0b38aa5b93f255c37f8aa0d1762c80c0
Logan <loganzartman@live.com>
2024-12-26 20:26:47 -0600

    add util types

--- src/component.ts
+++ src/component.ts
@@ -1,9 +1,20 @@
-import type { ZodTypeAny } from 'zod';
+import type { ZodTypeAny, z } from 'zod';
 
 export type Component<TName extends string, TZodSchema extends ZodTypeAny> = {
   name: TName;
   schema: TZodSchema;
 };
+
+export type ComponentSchema<TComponent extends Component<string, ZodTypeAny>> =
+  TComponent extends Component<any, infer TZodSchema> ? TZodSchema : never;
+
+export type ComponentName<TComponent extends Component<string, ZodTypeAny>> =
+  TComponent extends Component<infer TName, any> ? TName : never;
+
+export type ComponentData<TComponent extends Component<string, ZodTypeAny>> =
+  TComponent extends Component<any, infer TZodSchema>
+    ? z.infer<TZodSchema>
+    : never;
 
 export function component<
   TName extends string,
