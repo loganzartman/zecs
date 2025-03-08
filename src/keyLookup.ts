cb1fedfa576fa059145646f97637dcbf0c235e56
Logan <logan.zartman@utexas.edu>
2025-03-07 22:04:10 -0800

    update keyLookup, add query count()

--- src/keyLookup.ts
+++ src/keyLookup.ts
@@ -1,59 +1,75 @@
 import type { ECS, EntityLike } from './ecs';
 import type { Query } from './query';
 
-export type KeyLookup<TEntity extends EntityLike, TOutput extends TEntity> = {
+export type KeyLookup<TOutput extends EntityLike, TKey> = {
+  /** Update the set of matching entities. */
   update(): void;
-  get(e: TOutput): Readonly<Set<TOutput>>;
+  /** Count the number of entities that match the key. */
+  count(e: TKey): number;
+  /** Get the entities that match the key. */
+  get(e: TKey): Readonly<Set<TOutput>>;
 };
 
-export function keyLookup<TEntity extends EntityLike, TOutput extends TEntity>(
+/**
+ * Create a key lookup for a query.
+ *
+ * @param ecs The ECS instance.
+ * @param query The query to look up entities for.
+ * @param keyFn A function that takes an entity and returns an iterable of keys.
+ * @returns A key lookup object
+ */
+export function keyLookup<
+  TEntity extends EntityLike,
+  TOutput extends TEntity,
+  TKey extends Partial<TOutput>,
+>(
   ecs: ECS<TEntity>,
   query: Query<TEntity, TOutput>,
-  keyFn: (e: TOutput) => string | string[],
-): KeyLookup<TEntity, TOutput> {
+  keyFn: (e: TKey) => Iterable<string>,
+): KeyLookup<TOutput, TKey> {
   const empty = new Set();
-  const lut = new Map<string, Set<TOutput>>();
+  const kv = new Map<string, Set<TOutput>>();
 
-  function update() {
-    for (const set of lut.values()) {
-      set.clear();
-    }
+  const result: KeyLookup<TOutput, TKey> = {
+    get(e) {
+      const result = new Set<TOutput>();
+      const keys = keyFn(e as TKey);
+      for (const key of keys) {
+        for (const item of kv.get(key) ?? empty) {
+          result.add(item as TOutput);
+        }
+      }
+      return result;
+    },
 
-    for (const e of query.query(ecs)) {
-      const keys = keyFn(e);
-      if (Array.isArray(keys)) {
+    count(e) {
+      let result = 0;
+      const keys = keyFn(e as TKey);
+      for (const key of keys) {
+        for (const _ of kv.get(key) ?? empty) {
+          ++result;
+        }
+      }
+      return result;
+    },
+
+    update() {
+      for (const set of kv.values()) {
+        set.clear();
+      }
+
+      for (const e of query.query(ecs)) {
+        const keys = keyFn(e as unknown as TKey);
         for (const key of keys) {
-          const items = lut.get(key) ?? new Set();
+          const items = kv.get(key) ?? new Set();
+          kv.set(key, items);
           items.add(e);
-          lut.set(key, items);
         }
-      } else {
-        const key = keys;
-        const items = lut.get(key) ?? new Set();
-        items.add(e);
-        lut.set(key, items);
       }
-    }
-  }
-  update();
-
-  return {
-    update,
-    get(e: TOutput): Readonly<Set<TOutput>> {
-      const keys = keyFn(e);
-
-      if (Array.isArray(keys)) {
-        const result = new Set<TOutput>();
-        for (const key of keys) {
-          for (const item of lut.get(key) ?? empty) {
-            result.add(item as TOutput);
-          }
-        }
-        return result;
-      }
-
-      const key = keys;
-      return (lut.get(key) as Set<TOutput>) ?? empty;
     },
   };
+
+  result.update();
+
+  return result;
 }
